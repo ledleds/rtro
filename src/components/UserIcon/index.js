03a72cba43ee1cf9059d0e36b8fb3bf7ffc08431
ledleds <victorialedsom@gmail.com>
2020-07-19 23:02:42 +0100

    Read and unready icons

--- src/components/UserIcon/index.js
+++ src/components/UserIcon/index.js
@@ -1,9 +1,10 @@
 import { h } from 'preact';
 import style from './style.css';
 
-const UserIcon = ({initial, mainUser}) => {
+const UserIcon = ({initial, mainUser, ready}) => {
   return (
     <div class={`${style.container} ${mainUser ? style.mainUser : null}`}>
+      {!mainUser && <div class={`${style.status} ${ready ? style.ready : style.unready}`} />}
       <div class={style.initial}>
         {initial}
       </div>
